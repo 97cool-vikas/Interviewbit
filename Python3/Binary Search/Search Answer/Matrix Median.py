873b4807a7b7d829856f35a4a787f11966a55550
97cool-vikas <52127656+97cool-vikas@users.noreply.github.com>
2021-03-29 19:41:27 +0530

    Create Matrix Median.py

---
+++ Python3/Binary Search/Search Answer/Matrix Median.py
@@ -0,0 +1,59 @@
+'''
+Given a matrix of integers A of size N x M in which each row is sorted.
+
+Find an return the overall median of the matrix A.
+
+Note: No extra memory is allowed.
+
+Note: Rows are numbered from top to bottom and columns are numbered from left to right.
+
+
+
+Input Format
+
+The first and only argument given is the integer matrix A.
+Output Format
+
+Return the overall median of the matrix A.
+Constraints
+
+1 <= N, M <= 10^5
+1 <= N*M  <= 10^6
+1 <= A[i] <= 10^9
+N*M is odd
+For Example
+
+Input 1:
+    A = [   [1, 3, 5],
+            [2, 6, 9],
+            [3, 6, 9]   ]
+Output 1:
+    5
+Explanation 1:
+    A = [1, 2, 3, 3, 5, 6, 6, 9, 9]
+    Median is 5. So, we return 5.
+
+Input 2:
+    A = [   [5, 17, 100]    ]
+Output 2:
+    17 ``` Matrix=
+```
+'''
+class Solution:
+    # @param A : list of list of integers
+    # @return an integer
+    def findMedian(self, A):
+        def arr_median(arr):
+            
+            arr = sorted(arr)
+            median_index = (len(arr)+1)//2
+            return arr[median_index-1]
+            
+        if len(A)==0:
+            return []
+        
+        arr = []
+        for row in A:
+            arr.extend(row) 
+        
+        return arr_median(arr)
